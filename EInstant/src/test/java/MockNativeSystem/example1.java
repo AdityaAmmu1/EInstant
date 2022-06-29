d744e6f8eb63520f4ce99e9748451fc48e6a2b7a
Karpagakamatchi <karpagakamatchi.sethuraman@igt.com>
2022-06-29 20:44:57 +0530

    MNS-Colorcubes

--- EInstant/src/test/java/MockNativeSystem/example1.java
+++ EInstant/src/test/java/MockNativeSystem/example1.java
@@ -6,7 +6,7 @@ import java.util.Set;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.sikuli.script.FindFailed;
 import org.sikuli.script.Pattern;
 import org.sikuli.script.Screen;
@@ -21,31 +21,31 @@ public class example1{
 	static String absPath = file.getAbsolutePath();
 	@org.testng.annotations.BeforeTest
 	public void BeforeTest() throws InterruptedException{
-			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
 			driver.get("http://mns-ps02.lab.wagerworks.com:8080/MNS2/login.html");
 			driver.manage().window().maximize();
-			driver.findElement(By.xpath("//*[@id=\"userName\"]")).sendKeys("aditest1");
-			driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("123456");
+			driver.findElement(By.xpath("//*[@id=\"userName\"]")).sendKeys("0012");
+			driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("12345");
 			driver.findElement(By.xpath("//*[@id=\"loginBtn\"]")).click();
 			Thread.sleep(30000);
 			driver.findElement(By.cssSelector("#ixfInstantWin > div > div.inner > h3")).click();// click on ixf Instant win tag
 			Thread.sleep(15000);
 			driver.findElement(By.xpath("//*[@id=\"resources\"]/form/div/div[1]/div/span/span[1]/span/span[2]")).click();// Click on drop down RGS server 
 			Thread.sleep(1000);
-			driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys("RGS-CUST03" +Keys.ENTER);// enter server name
+			driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys("RGS-CUST05" +Keys.ENTER);// enter server name
 		    Thread.sleep(1000);
 		    driver.findElement(By.xpath("//*[@id=\"resources\"]/form/div/div[2]/div/span/span[1]/span/span[2]")).click();
 		    Thread.sleep(1000);
-		    driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys("GALT" +Keys.ENTER);
+		    driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys("PSo1" +Keys.ENTER);
 		    Thread.sleep(1000);
 		    driver.findElement(By.xpath("//*[@id=\"resources\"]/form/div/div[3]/div/span/span[1]/span/span[2]")).click();
 		    Thread.sleep(1000);
-		    driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys("GG02" +Keys.ENTER);
+		    driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys("Demo" +Keys.ENTER);
 		    Thread.sleep(1000);
-		    driver.findElement(By.xpath("//*[@id=\"skbEInstant_player\"]")).sendKeys("aditest1" +Keys.ENTER);
+		    driver.findElement(By.xpath("//*[@id=\"skbEInstant_player\"]")).sendKeys("kam" +Keys.ENTER);
 		    Thread.sleep(1000);
-		    driver.findElement(By.xpath("//*[@id=\"skbEInstant_password\"]")).sendKeys("123456" +Keys.ENTER);
+		    driver.findElement(By.xpath("//*[@id=\"skbEInstant_password\"]")).sendKeys("12345" +Keys.ENTER);
 		    Thread.sleep(1000);
 		    driver.findElement(By.xpath("//*[@id=\"resources\"]/form/div/div[6]/div/span/span[1]/span/span[2]")).click();
 		    Thread.sleep(1000);
@@ -53,7 +53,7 @@ public class example1{
 		    Thread.sleep(1000);
 		    driver.findElement(By.xpath("//*[@id=\"resources\"]/form/div/div[7]/div/span/span[1]/span/span[2]")).click();
 		    Thread.sleep(1000);
-		    driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys("GB" +Keys.ENTER);
+		    driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys("US" +Keys.ENTER);
 		    Thread.sleep(1000);
 		    driver.findElement(By.xpath("//*[@id=\"resources\"]/form/div/div[8]/div/span/span[1]/span/span[2]")).click();
 		    Thread.sleep(1000);
@@ -64,7 +64,7 @@ public class example1{
 	  }
 	 @Test
 	 public void Ruby() throws FindFailed, InterruptedException{
-		  driver.findElement(By.xpath("/html/body/div/div/div[1]/div[2]/div/section[2]/div/div/div/div[1]/div/div[2]/div/div/input")).sendKeys("ruby");
+		  driver.findElement(By.xpath("/html/body/div/div/div[1]/div[2]/div/section[2]/div/div/div/div[1]/div/div[2]/div/div/input")).sendKeys("Color Cubes");
 		  Thread.sleep(5000);
 		  driver.findElement(By.xpath("//*[@id=\"skb-gamelist-wrapper\"]/div[1]/table/tbody/tr/td[4]/table/tbody/tr[3]/td[2]/a[2]")).click();
 		   
@@ -78,22 +78,26 @@ public class example1{
 		    }
 		    driver.manage().window().maximize();
 		Screen screen = new Screen();
-	   	Pattern sound = new Pattern(absPath+"\\MNS\\ruby\\sound1.png");
+	   	Pattern sound = new Pattern(absPath+"\\MNS\\colorcubes\\audio.png");
 	   	screen.wait(sound,40000);
 	   	screen.click(sound);
 	   	
-	   	Pattern buy = new Pattern(absPath+"\\MNS\\ruby\\buy2.png");
+	   	Pattern close = new Pattern(absPath+"\\MNS\\colorcubes\\close.png");
+	   	screen.wait(close,40000);
+	   	screen.click(close);
+	   	  	
+	   	Pattern inc = new Pattern(absPath+"\\MNS\\colorcubes\\increase.png");
+	   	screen.wait(inc,20000);
+	   	screen.click(inc);
+	   	
+	   	Pattern buy = new Pattern(absPath+"\\MNS\\colorcubes\\buy.png");
 	   	screen.wait(buy,20000);
 	   	screen.click(buy);
-	   	
-	   	Pattern revealAll = new Pattern(absPath+"\\MNS\\ruby\\revealAll3.png");
-	   	screen.wait(revealAll,20000);
-	   	screen.click(revealAll);
-	   	Thread.sleep(10000);
-	   	
-	   	Pattern playAgain4 = new Pattern(absPath+"\\MNS\\ruby\\playAgain4.png");
-	   	screen.wait(playAgain4,20000);
-	   	screen.click(playAgain4);
+	   		   	
+	   	Pattern revealall = new Pattern(absPath+"\\MNS\\colorcubes\\autoreveal.png");
+	   	screen.wait(revealall,20000);
+	   	screen.click(revealall);
+	   	Thread.sleep(40000);
 	   	
 	   	driver.close();
 	   	driver.switchTo().window(parentID); //switching back to Parent window
