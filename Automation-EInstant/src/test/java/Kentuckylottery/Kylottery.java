971ac662f41d34410bca315118e1f05b7dcf29e9
Karpagakamatchi <karpagakamatchi.sethuraman@igt.com>
2022-07-29 11:44:37 +0530

    KYLottery

--- Automation-EInstant/src/test/java/Kentuckylottery/Kylottery.java
+++ Automation-EInstant/src/test/java/Kentuckylottery/Kylottery.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.sikuli.script.FindFailed;
 import org.sikuli.script.Pattern;
 import org.sikuli.script.Screen;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -23,12 +24,12 @@ public class Kylottery {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();		
 	}
-	@Test
+	@Test//rub again
 	public void Multiplythecash() throws FindFailed
 	{
 		driver.get("https://rgs-az1cust01.lab.wagerworks.com/skb/gateway?currencycode=USD&countrycode=US&locale=en_US&wagerType=TRY&language=en_EN&softwareid=300-2134-006&channel=INT&presenttype=STD&technology=HTML&nscode=KYLT&skincode=KY99");
 		Screen s = new Screen();
-		Pattern info = new Pattern(absPath +"\\KYlottery\\Multiply thecash\\info.png").similar(0.4);
+		Pattern info = new Pattern(absPath +"\\KYlottery\\Multiply thecash\\info.png").similar(0.5);
 		s.wait(info,40000);
 		s.click(info);
 		Pattern audio = new Pattern(absPath +"\\KYlottery\\Multiply thecash\\audio.png").similar(0.4);
@@ -37,20 +38,20 @@ public class Kylottery {
 		Pattern ok = new Pattern(absPath +"\\KYlottery\\Multiply thecash\\ok.png").similar(0.4);
 		s.wait(ok,40000);
 		s.click(ok);
-		Pattern decrease = new Pattern(absPath +"\\KYlottery\\Multiply thecash\\decrease.png").similar(0.4);
+		Pattern decrease = new Pattern(absPath +"\\KYlottery\\Multiply thecash\\decrease.png").similar(0.6);
 		s.wait(decrease,40000);
 		s.click(decrease);
 		Pattern t = new Pattern(absPath +"\\KYlottery\\Multiply thecash\\Try.png").similar(0.4);
 		s.wait(t,40000);
 		s.click(t);
-		Pattern revealall = new Pattern(absPath +"\\KYlottery\\Multiply thecash\\revealall.png").similar(0.4);
+		Pattern revealall = new Pattern(absPath +"\\KYlottery\\Multiply thecash\\revealall.png").similar(0.5);
 		s.wait(revealall,40000);
 		s.click(revealall);
 		Pattern ta = new Pattern(absPath +"\\KYlottery\\Multiply thecash\\tryagain.png").similar(0.4);
 		s.wait(ta,40000);
 		
 	}
-	@Test
+	@Test//run again
 	public void Ruby7() throws FindFailed
 	{
 		driver.get("https://rgs-az1cust01.lab.wagerworks.com/skb/gateway?currencycode=USD&countrycode=US&locale=en_US&wagerType=TRY&language=en_EN&softwareid=300-2171-008&channel=INT&presenttype=STD&technology=HTML&nscode=KYLT&skincode=KY99");
@@ -58,7 +59,7 @@ public class Kylottery {
 		Pattern info = new Pattern(absPath +"\\KYlottery\\Ruby7\\info.png").similar(0.6);
 		s.wait(info,40000);
 		s.click(info);
-		Pattern audio = new Pattern(absPath +"\\KYlottery\\Ruby7\\audio.png").similar(0.6);
+		Pattern audio = new Pattern(absPath +"\\KYlottery\\Ruby7\\audio.png").similar(0.3);
 		s.wait(audio,40000);
 		s.click(audio);
 		Pattern ok = new Pattern(absPath +"\\KYlottery\\Ruby7\\ok.png").similar(0.4);
@@ -70,11 +71,48 @@ public class Kylottery {
 		Pattern t = new Pattern(absPath +"\\KYlottery\\Ruby7\\Try.png").similar(0.4);
 		s.wait(t,40000);
 		s.click(t);
-		Pattern revealall = new Pattern(absPath +"\\KYlottery\\Ruby7\\revealall.png").similar(0.4);
+		Pattern revealall = new Pattern(absPath +"\\KYlottery\\Ruby7\\revealall.png").similar(0.5);
 		s.wait(revealall,40000);
 		s.click(revealall);
 		Pattern ta = new Pattern(absPath +"\\KYlottery\\Ruby7\\tryagain.png").similar(0.4);
 		s.wait(ta,40000);
 		
 	}
+	@Test
+	public void payoutpeak() throws FindFailed
+	{
+		driver.get("https://rgs-az1cust01.lab.wagerworks.com/skb/gateway?currencycode=USD&countrycode=US&locale=en_US&wagerType=TRY&language=en_EN&softwareid=300-2172-004&channel=INT&presenttype=STD&technology=HTML&nscode=KYLT&skincode=KY99");
+		Screen s = new Screen();
+		//Pattern info = new Pattern(absPath +"\\KYlottery\\\\info.png").similar(0.6);
+		//s.wait(info,40000);
+		//s.click(info);
+		Pattern audio = new Pattern(absPath +"\\KYlottery\\Payoutpeak\\audio.png").similar(0.5);
+		s.wait(audio,40000);
+		s.click(audio);
+		Pattern ok = new Pattern(absPath +"\\KYlottery\\Payoutpeak\\ok.png").similar(0.4);
+		s.wait(ok,40000);
+		s.click(ok);
+		Pattern decrease = new Pattern(absPath +"\\KYlottery\\Payoutpeak\\decrease.png").similar(0.6);
+		s.wait(decrease,40000);
+		s.click(decrease);
+		Pattern t = new Pattern(absPath +"\\KYlottery\\Payoutpeak\\try.png").similar(0.4);
+		s.wait(t,40000);
+		s.click(t);
+		Pattern ok1 = new Pattern(absPath +"\\KYlottery\\Payoutpeak\\ok-inside.png").similar(0.4);
+		s.wait(ok1,40000);
+		s.click(ok1);
+		Pattern revealall = new Pattern(absPath +"\\KYlottery\\Payoutpeak\\Autoclimb.png").similar(0.5);
+		s.wait(revealall,40000);
+		s.click(revealall);
+		Pattern ta = new Pattern(absPath +"\\KYlottery\\Payoutpeak\\tryagain.png").similar(0.4);
+		s.wait(ta,40000);
+		
+	}
+	
+
+@AfterTest
+public void aftertest()
+{
+driver.quit();	
+}
 }
